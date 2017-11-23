a44eedcd9a65f8455dff0ce423b41494a189309e
Jake Spilchuk (jrs206) <jrs206@mail.usask.ca>
2017-11-22 18:55:02 -0600

    Upload New File

---
+++ js/otherItems.js
@@ -0,0 +1,47 @@
+var otherItems = [
+	{
+		"title":"A Head Full of Dreams<br>Custom Printing Baseball Caps",
+        "description":"This Running Cap Adjustable Snapback Flexfit Flat Cap ,Accept To Figure Custom. Simple & Stylish Design. "
+		+  "Suit For Youths,Boys,Girls,Women,Men. It's Perfect For A Variety Of Occasions. ",
+        "photoLocation":"img/items1.jpg",
+        "saleLocation":"https://www.amazon.ca/Coldplay-Dreams-Custom-Printing-Baseball/dp/B01HNPNPLK/ref=sr_1_4?ie=UTF8&qid=1511131334&sr=8-4&keywords=coldplay",
+        "price":"11.59"},
+	{
+		"title":"A Head Full of Dreams<br>Cool Beanie Hat Cap",
+		"description:":"New Warm Fashion Design, Very Popular. Both Suit For Men And Women, Also Fits For Teens. This Trendy And Fashion Hat Will Be Your Favorite "
+		+ "Choice For Accessorizing. A Perfect Gift To Yourself Or Friends.",
+		"photoLocation":"img/items2.jpg",
+		"saleLocation":"https://www.amazon.ca/Coldplay-Head-Full-Dreams-Beanie/dp/B01NAAQ7Q8/ref=sr_1_9?ie=UTF8&qid=1511385515&sr=8-9&keywords=coldplay+hat",
+		"price":"13.00"},
+	{
+		"title":"Sky Full of Stars<br>Soft T-Shirt",
+		"description:":"This soft t-shirt from Coldplay features artwork inspired by the group's track, A Sky Full of Stars from the album Ghost Stories. The bottom "
+		+ "features group logo and A Sky Full of Stars text. A great design that's perfect for any fan of Coldplay! ",
+		"photoLocation":"img/items3.jpeg",
+		"saleLocation":"https://www.amazon.ca/Coldplay-Full-Stars-Soft-T-Shirt/dp/B00XWIMMTY/ref=sr_1_17?ie=UTF8&qid=1511132799&sr=8-17&keywords=coldplay",
+		"price":"13.50"},
+	{
+		"title":"I AM YOU<br>T-Shirt",
+		"description:":"I AM YOU T-shirt",
+		"photoLocation":"img/items4.jpg",
+		"saleLocation":"https://coldplay.fanfire.com/cgi-bin/WebObjects/Store.woa/wa/product?sku=COL73166",
+		"price":"24.95"},
+	{
+		"title":"Ghost Stories<br>IPhone 6 Plus Case",
+		"description:":"Silicone iPhone Case with the 'Ghost Stories' album art printed on the back. Will fit only iPhone 6 Plus models.",
+		"photoLocation":"img/items5.jpg",
+		"saleLocation":"https://coldplay.fanfire.com/cgi-bin/WebObjects/Store.woa/wa/product?sourceCode=COLWEBUKWUSD&sku=COL63046",
+		"price":"12.95"},
+	{
+		"title":"Splatter Photo North American 2017 Tour<br>Black T-Shirt",
+		"description:":"Black, 100% cotton, with a paint splatter photo of the band on the front and the stops on the North American 2017 leg printed on the back.",
+		"photoLocation":"img/items6.jpg",
+		"saleLocation":"https://coldplay.fanfire.com/cgi-bin/WebObjects/Store.woa/wa/product?sku=COL74520",
+		"price":"39.95"},
+	{
+		"title":"Flower Of Life<br>Mug",
+		"description:":"Black ceramic mug has the Flower of Life graphic printed on one side and the new Coldplay logo on the other.",
+		"photoLocation":"img/items7.jpg",
+		"saleLocation":"https://coldplay.fanfire.com/cgi-bin/WebObjects/Store.woa/wa/product?sku=COL68615",
+		"price":"19.95"}
+];
