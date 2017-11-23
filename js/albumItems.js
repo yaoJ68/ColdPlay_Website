3dec2580fd13b56b0ee35a7afcfc1782f5e5c839
Jake Spilchuk (jrs206) <jrs206@mail.usask.ca>
2017-11-22 18:56:38 -0600

    New name for items.js. Distinguishes it from otherItems

---
+++ js/albumItems.js
@@ -0,0 +1,77 @@
+var albumItems = [
+    {
+        "title":"Kaleidoscope EP(2017)",
+        "description":"COLDPLAY’S UPCOMING UPCOMING KALEIDOSCOPE EP ANNOUNCED The EP was produced by Rik Simpson with Daniel Green and Bill Rahko and features a new live version of Coldplay’s "
+        + "hit collaboration with The Chainsmokers, Something Just Like This, recorded in Tokyo in April. It also includes Miracles (Someone Special), a collaboration with Detroit rapper Big Sean. "
+        + "Another new song, A L I E N S, was written with Brian Eno, and co-produced by Eno and Markus Dravs (Arcade Fire, Mumford & Sons, Kings Of Leon). The EP is the sister release to the "
+        + "band’s acclaimed A Head Full Of Dreams album, which has sold more than 5 million copies around the world since its December 2015 release. Brian Eno and Peter Chivers have also created "
+        + "a new app to accompany the song Hypnotised from the EP. The app allows fans to play along with the track, which was released in February, at shows or at home. It can be downloaded for "
+        + "free via coldplay.com/apps.",
+        "photoLocation":"img/album1.jpg",
+        "backCover":"img/album1back.jpeg",
+		"audioLoction":"audio/album1.mp3",
+        "saleLocation":"https://www.amazon.ca/Kaleidoscope-EP-Coldplay/dp/B0714JF669/ref=sr_1_1_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-1&keywords=coldplay",
+        "price":"8.00"},
+    {
+        "title":"A Head Full Of Dreams(2015)",
+        "description":"This is the seventh studio album from multi-platinum, Grammy Award winning artist Coldplay.",
+        "photoLocation":"img/album2.jpg",
+        "backCover":"img/album2back.jpg",
+        "audioLoction":"audio/album2.mp3",
+        "saleLocation":"https://www.amazon.ca/Head-Full-Dreams-Coldplay/dp/B017IS0HY0/ref=sr_1_2_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-2&keywords=coldplay",
+        "price":"12.88"},
+    {
+        "title":"Ghost Stories(2014)",
+        "description":"Superstar band Coldplay presents their sixth album, GHOST STORIES. The album is preceded by three singles: 'Magic', released in March 2014, 'Midnight', released in "
+        + "April 2014, and 'A Sky Full of Stars', released in May 2014.",
+        "photoLocation":"img/album4.jpg",
+        "backCover":"img/album3back.jpg",
+        "audioLoction":"audio/album3.mp3",
+        "saleLocation":"https://www.amazon.ca/Ghost-Stories-Coldplay/dp/B00IQE4NUK/ref=sr_1_10_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-10&keywords=coldplay",
+        "price":"12.88"},
+    {
+        "title":"Parachutes(2013)",
+        "description":"Their Eagerly Awaited Debut Full Length. Garnered Rave Reviews on their Nme Tour with Shack, Gomez and Bellatrix. Often Compared to Radiohead and Jeff Buckley.",
+        "photoLocation":"img/album6.jpg",
+        "backCover":"img/album4back.jpg",
+        "audioLoction":"audio/album4.mp3",
+        "saleLocation":"https://www.amazon.ca/Parachutes-Coldplay/dp/B00004U9MS/ref=sr_1_4_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-4&keywords=coldplay",
+        "price":"10.00"},
+    {
+        "title":"Viva La Vida or Death and All His Friends(2013)",
+        "description":"Limited Asian two disc (CD + NTSC/Region 0 DVD) Tour Edition includes a bonus DVD that contains six music videos to the singles off the album: 'Strawberry Swing', "
+        + "'Violet Hill', 'Viva La Vida', 'Lost!', 'Lovers In Japan' and 'Life In Technicolor II'. The 2008 album comprises 10 brand new tracks, recorded in London, Barcelona and New "
+        + "York with producers Brian Eno and Markus Dravs. Viva La Vida followed the hugely successful album X&Y, which has sold 10 million copies since its release in 2005. EMI.",
+        "photoLocation":"img/album7.jpg",
+        "backCover":"img/album5back.jpg",
+        "audioLoction":"audio/album5.mp3",
+        "saleLocation":"https://www.amazon.ca/Viva-Vida-Death-All-Friends/dp/B00180OSXG/ref=sr_1_12_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-12&keywords=coldplay",
+        "price":"18.48"},
+    {
+        "title":"X&Y(2013)",
+        "description":"Very limited special edition 2 CD Dutch pressing with special package and slipcase. The first disc is the same as the US pressing of X & Y while the bonus CD features "
+        + "all of the B-Sides released thus far off of singles from the album, 'Things I Don't Understand', 'Proof', 'The World Turned Upside Down', 'Pour Me' (Live at the Hollywood Bowl), "
+        + "'Sleeping Sun' & 'Gravity'. Capitol. 2006.",
+        "photoLocation":"img/album8.jpg",
+        "backCover":"img/album6back.jpg",
+        "audioLoction":"audio/album6.mp3",
+        "saleLocation":"https://www.amazon.ca/X-Y-Hidden-Track-Coldplay/dp/B0009AM5JA/ref=sr_1_13_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-13&keywords=coldplay",
+        "price":"4.59"},
+    {
+        "title":"Mylo Xyloto(2011)",
+        "description":"Please take notice that this album ships with one of two different cover art images displayed. Each album has both; to switch your displayed artwork, "
+        + "please flip the insert over.",
+        "photoLocation":"img/album3.jpg",
+        "backCover":"img/album7back.jpg",
+        "audioLoction":"audio/album7.mp3",
+        "saleLocation":"https://www.amazon.ca/Mylo-Xyloto-Coldplay/dp/B0053YGYO4/ref=sr_1_3?ie=UTF8&qid=1510013406&sr=8-3&keywords=coldplay",
+        "price":"4.66"},
+    {
+        "title":"A Rush of Blood to the Head(2002)",
+        "description":"2002 sophomore album from the British Pop/Rockers led by singer, songwriter and pianist Chris Martin. Includes the massive hit singles 'Clocks' and 'The Scientist'.",
+        "photoLocation":"img/album5.jpg",
+        "backCover":"img/album8back.jpg",
+        "audioLoction":"audio/album8.mp3",
+        "saleLocation":"https://www.amazon.ca/Rush-Blood-Head-Coldplay/dp/B000069AUI/ref=sr_1_7_mimg_1_music_display_on_website?ie=UTF8&qid=1510013406&sr=8-7&keywords=coldplay",
+        "price":"8.97"}
+];
