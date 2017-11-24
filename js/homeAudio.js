6535c5e1fba47f458361b9ed5f05c81b5c8b9bba
Yao Jiang <yaj889@mail.usask.ca>
2017-11-24 00:51:37 -0600

    -Home page: changed music player position to the bottom of the page, added gradient background to it, added previous/next song button to it and made them functionally work

--- js/homeAudio.js
+++ js/homeAudio.js
@@ -1,11 +1,11 @@
 
 function play()
 {
-    if (document.getElementById("cover-audio").getAttribute("src") == "img/player.png") {
+    if (document.getElementById("cover-audio").getAttribute("src") === "img/player.png") {
         document.getElementById("cover-audio").src = "img/pause.png";
         document.getElementById("audio-play").play();
     }
-    else if(document.getElementById("cover-audio").getAttribute("src") == "img/pause.png") {
+    else if(document.getElementById("cover-audio").getAttribute("src") === "img/pause.png") {
         document.getElementById("cover-audio").src = "img/player.png";
         document.getElementById("audio-play").pause();
     }
@@ -13,4 +13,15 @@ function play()
         alert("Image Not Located for play()");
     }
     return false;
+}
+
+//instance for next()
+var songlist = ["audio/Yellow.mp3","audio/SJLT.mp3"];
+var curSongIndex = 0;
+
+function next(){
+    curSongIndex = 1 - curSongIndex;
+    document.getElementById("current-audio").src = songlist[curSongIndex];
+    document.getElementById("audio-play").load();
+    document.getElementById("audio-play").play();
 }
