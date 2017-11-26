71106c3b5765952d6e1466f28a5219dd56eab10a
Fatima Ahmad (faa497) <anam.ahmad@usask.ca>
2017-11-25 22:09:03 -0600

    Update storePlayAudio.js: added audio over hover on album

--- js/storePlayAudio.js
+++ js/storePlayAudio.js
@@ -1,3 +1,112 @@
+var audio1 = document.getElementById("1");
+var item1 = document.getElementById("album1");
 
-var audio = document.getElementById("storeAudio");
-audio.play();
+item1.addEventListener("mouseover", playMusic1, false);
+item1.addEventListener("mouseout", stopMusic1, false);
+
+function playMusic1() {
+    audio1.play();
+}
+
+function stopMusic1() {
+    audio1.pause();
+}
+
+
+var audio2 = document.getElementById("2");
+var item2 = document.getElementById("album2");
+
+item2.addEventListener("mouseover", playMusic2, false);
+item2.addEventListener("mouseout", stopMusic2, false);
+
+function playMusic2() {
+    audio2.play();
+}
+
+function stopMusic2() {
+    audio2.pause();
+}
+
+var audio3 = document.getElementById("3");
+var item3 = document.getElementById("album3");
+
+item3.addEventListener("mouseover", playMusic3, false);
+item3.addEventListener("mouseout", stopMusic3, false);
+
+function playMusic3() {
+    audio3.play();
+}
+
+function stopMusic3() {
+    audio3.pause();
+}
+
+var audio4 = document.getElementById("4");
+var item4 = document.getElementById("album4");
+
+item4.addEventListener("mouseover", playMusic4, false);
+item4.addEventListener("mouseout", stopMusic4, false);
+
+function playMusic4() {
+    audio4.play();
+}
+
+function stopMusic4() {
+    audio4.pause();
+}
+
+var audio5 = document.getElementById("5");
+var item5 = document.getElementById("album5");
+
+item5.addEventListener("mouseover", playMusic5, false);
+item5.addEventListener("mouseout", stopMusic5, false);
+
+function playMusic5() {
+    audio5.play();
+}
+
+function stopMusic5() {
+    audio5.pause();
+}
+
+var audio6 = document.getElementById("6");
+var item6 = document.getElementById("album6");
+
+item6.addEventListener("mouseover", playMusic6, false);
+item6.addEventListener("mouseout", stopMusic6, false);
+
+function playMusic6() {
+    audio6.play();
+}
+
+function stopMusic6() {
+    audio6.pause();
+}
+
+var audio7 = document.getElementById("7");
+var item7 = document.getElementById("album7");
+
+item7.addEventListener("mouseover", playMusic7, false);
+item7.addEventListener("mouseout", stopMusic7, false);
+
+function playMusic7() {
+    audio7.play();
+}
+
+function stopMusic7() {
+    audio7.pause();
+}
+
+var audio8 = document.getElementById("8");
+var item8 = document.getElementById("album8");
+
+item8.addEventListener("mouseover", playMusic8, false);
+item8.addEventListener("mouseout", stopMusic8, false);
+
+function playMusic8() {
+    audio8.play();
+}
+
+function stopMusic8() {
+    audio8.pause();
+}
